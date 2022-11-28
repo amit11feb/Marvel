b9ed7fbbf730ff2687756b2c450b87a315de19f2
Chaithra S <Chaithra1.S@lntinfotech.com>
2022-11-29 03:20:07 +0530

    responsive

--- frontend/src/app/user.service.ts
+++ frontend/src/app/user.service.ts
@@ -7,14 +7,22 @@ export class UserService {
   constructor(private _http: HttpClient) { }
 
   register(body: any) {
+<<<<<<< HEAD
     return this._http.post('http://127.0.0.1:5000/users/register', body, {
+=======
+    return this._http.post('http://127.0.0.1:3000/users/register', body, {
+>>>>>>> 280bb8ca09b28068928a3c9c8c23eaacfa05be69
       observe: 'body',
       headers: new HttpHeaders().append('Content-Type', 'application/json')
     });
   }
 
   login(body: any) {
+<<<<<<< HEAD
     return this._http.post('http://127.0.0.1:5000/users/login', body, {
+=======
+    return this._http.post('http://127.0.0.1:3000/users/login', body, {
+>>>>>>> 280bb8ca09b28068928a3c9c8c23eaacfa05be69
       observe: 'body',
       withCredentials: true,
       headers: new HttpHeaders().append('Content-Type', 'application/json')
@@ -22,7 +30,11 @@ export class UserService {
   }
 
   user() {
+<<<<<<< HEAD
     return this._http.get('http://127.0.0.1:5000/users/user', {
+=======
+    return this._http.get('http://127.0.0.1:3000/users/user', {
+>>>>>>> 280bb8ca09b28068928a3c9c8c23eaacfa05be69
       observe: 'body',
       withCredentials: true,
       headers: new HttpHeaders().append('Content-Type', 'application/json')
@@ -30,7 +42,11 @@ export class UserService {
   }
 
   logout() {
+<<<<<<< HEAD
     return this._http.get('http://127.0.0.1:5000/users/logout', {
+=======
+    return this._http.get('http://127.0.0.1:3000/users/logout', {
+>>>>>>> 280bb8ca09b28068928a3c9c8c23eaacfa05be69
       observe: 'body',
       withCredentials: true,
       headers: new HttpHeaders().append('Content-Type', 'application/json')
@@ -48,17 +64,28 @@ export class UserService {
     
 console.log(formData);
 
+<<<<<<< HEAD
 return this._http.post('http://127.0.0.1:5000/property/addProperty', formData);
 
 
     // return this._http.post('http://127.0.0.1:5000/property/addProperty', formData, {
+=======
+return this._http.post('http://127.0.0.1:3000/property/addProperty', formData);
+
+
+    // return this._http.post('http://127.0.0.1:3000/property/addProperty', formData, {
+>>>>>>> 280bb8ca09b28068928a3c9c8c23eaacfa05be69
     //   observe: 'body',
     //   headers: new HttpHeaders().append('Content-Type', 'application/json').append('Content-Type', 'multipart/form-data')
     // });
   }
 
   getPropertyList() {
+<<<<<<< HEAD
     return this._http.get('http://127.0.0.1:5000/property/getProperty', {
+=======
+    return this._http.get('http://127.0.0.1:3000/property/getProperty', {
+>>>>>>> 280bb8ca09b28068928a3c9c8c23eaacfa05be69
       observe: 'body',
       headers: new HttpHeaders().append('Content-Type', 'application/json')
     });
